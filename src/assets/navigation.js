246dd2a27b4be5e16de25afef0a55831feadc407
Pia <94532135+Pooleis1@users.noreply.github.com>
2026-05-16 14:31:12 +0200

    added impressum

--- src/assets/navigation.js
+++ src/assets/navigation.js
@@ -3,7 +3,7 @@ const NavigationComponent = {
   template: `
     <!-- ─── NAVIGATION ─── -->
     <nav :class="{ scrolled: isScrolled }">
-        <a href="#hero" class="logo">
+        <a href="index.html#hero" class="logo">
         <img src="./src/assets/Logo_Vollversion.png" alt="Firmenlogo">
         </a>
 
@@ -13,7 +13,6 @@ const NavigationComponent = {
         <li><a href="index.html#pricing">Preise</a></li>
         <li><a href="index.html#contact">Kontakt</a></li>
         <li><a href="index.html#about">Über Uns</a></li>
-        <li><a href="">Lizenz</a></li>
         <li><a href="">Datenschutzerklärung</a></li>
         <li><a href="">Impressum</a></li>
         </ul>
@@ -30,7 +29,6 @@ const NavigationComponent = {
         <a href="index.html#pricing"   @click="$emit('toggle-menu')">Preise</a>
         <a href="index.html#contact"   @click="$emit('toggle-menu')">Kontakt</a>
         <a href="index.html#about"     @click="$emit('toggle-menu')">Über Uns</a>
-        <a href="" @click="menuOpen=false">Lizenz</a>
         <a href="" @click="menuOpen=false">Datenschutzerklärung</a>
         <a href="" @click="menuOpen=false">Impressum</a>
     </div>
